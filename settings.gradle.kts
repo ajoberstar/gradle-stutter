86056e709037271cd8f3c8e863b20087e43f9c61
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-02-07 10:33:42 -0600

    Use latest rc of stutter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
 
     id("org.ajoberstar.grgit") version "4.1.1"
     id("org.ajoberstar.reckon") version "0.13.1"
-    id("org.ajoberstar.stutter") version "0.7.0-beta.2"
+    id("org.ajoberstar.stutter") version "0.7.0-rc.1"
 
     id("com.diffplug.spotless") version "6.2.1"
   }
